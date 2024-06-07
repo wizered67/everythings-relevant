0fd808dacb181c71370a32ed3416058b1a9b3f58
Adam Victor <wizered67@users.noreply.github.com>
2024-06-07 05:07:47 +0000

    Add basic clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Clock } from './Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello world 2!</h1>
+        <Clock/>
       </header>
     </div>
   );
